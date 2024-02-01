4b893c36161d5ec465b6ae4396284aee4b99f9d2
ramonpin <ramon.pin@gmail.com>
2024-02-01 13:24:51 +0100

    feat: Workaround for 404 status codes
    Also add status code custom pages

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,34 @@
 package com.example
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.plugins.statuspages.*
 
 fun Application.module() {
+
+    intercept(ApplicationCallPipeline.Fallback) {
+        if (call.isHandled) return@intercept
+        val status = call.response.status() ?: HttpStatusCode.NotFound
+        call.respond(status)
+    }
+
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) { call, status ->
+            call.respondText(text = "404: Page Not Found", status = status)
+        }
+    }
+
     routing {
         get("/") {
-            call.respondText("Hello, world!")
+            call.respondRedirect("main")
+        }
+
+        route("main") {
+            get {
+                call.respondText("Hello, world!")
+            }
         }
     }
 }
